4cb6aa60f0c29fa66dd6eba7550ce22449120c29
smcsoft <smcsoft@smcsoft.com.ar>
2021-01-10 08:38:18 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ejercicio.Global" Language="C#" %>
---
+++ Modelo/Instrumento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Instrumento
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+        public Tipo TipoInstrmento { get; set; }
+    }
+}
